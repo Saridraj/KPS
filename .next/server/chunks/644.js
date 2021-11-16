4811b6c743b72b2a4ce38b20cb58d89c1047ea58
Saridraj <saridrajsapsin@gmail.com>
2021-11-16 21:18:14 +0700

    init

---
+++ .next/server/chunks/644.js
@@ -0,0 +1,318 @@
+exports.id = 644;
+exports.ids = [644];
+exports.modules = {
+
+/***/ 2651:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(9297);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(2202);
+/* harmony import */ var _styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var next_Image__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6937);
+/* harmony import */ var next_Image__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_Image__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(1664);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(5282);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+
+
+
+function Footer() {
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+    className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_4___default().footer00),
+    children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+      className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_4___default().footer),
+      children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("div", {
+        className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_4___default().footer1),
+        children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("div", {
+          className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_4___default().footerbrand),
+          children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("center", {
+            children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx((next_Image__WEBPACK_IMPORTED_MODULE_1___default()), {
+              src: "/logo-kp.png",
+              width: 80,
+              height: 80
+            }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("h4", {
+              children: "\u0E42\u0E23\u0E07\u0E40\u0E23\u0E35\u0E22\u0E19\u0E42\u0E04\u0E01\u0E1B\u0E23\u0E07\u0E27\u0E34\u0E17\u0E22\u0E32\u0E04\u0E21"
+            }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("p", {
+              children: "\u0E2A\u0E33\u0E19\u0E31\u0E01\u0E07\u0E32\u0E19\u0E40\u0E02\u0E15\u0E1E\u0E37\u0E49\u0E19\u0E17\u0E35\u0E48\u0E01\u0E32\u0E23\u0E28\u0E36\u0E01\u0E29\u0E32\u0E21\u0E31\u0E18\u0E22\u0E21\u0E28\u0E36\u0E01\u0E29\u0E32\u0E40\u0E1E\u0E0A\u0E23\u0E1A\u0E39\u0E23\u0E13\u0E4C"
+            })]
+          })
+        })
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("div", {
+        className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_4___default().footer2),
+        children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("center", {
+          children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("ul", {
+            children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("li", {
+              children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx(next_link__WEBPACK_IMPORTED_MODULE_2__.default, {
+                href: "/",
+                children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("button", {
+                  children: "\u0E2B\u0E19\u0E49\u0E32\u0E41\u0E23\u0E01"
+                })
+              })
+            }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("li", {
+              children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx(next_link__WEBPACK_IMPORTED_MODULE_2__.default, {
+                href: "/school",
+                children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("button", {
+                  children: "\u0E02\u0E49\u0E2D\u0E21\u0E39\u0E25\u0E42\u0E23\u0E07\u0E40\u0E23\u0E35\u0E22\u0E19"
+                })
+              })
+            }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("li", {
+              children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx(next_link__WEBPACK_IMPORTED_MODULE_2__.default, {
+                href: "/personal",
+                children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("button", {
+                  children: "\u0E1A\u0E38\u0E04\u0E25\u0E32\u0E01\u0E23"
+                })
+              })
+            }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("li", {
+              children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx(next_link__WEBPACK_IMPORTED_MODULE_2__.default, {
+                href: "/student",
+                children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("button", {
+                  children: "\u0E02\u0E49\u0E2D\u0E21\u0E39\u0E25\u0E19\u0E31\u0E01\u0E40\u0E23\u0E35\u0E22\u0E19"
+                })
+              })
+            }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("li", {
+              children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx(next_link__WEBPACK_IMPORTED_MODULE_2__.default, {
+                href: "/activity",
+                children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("button", {
+                  children: "\u0E01\u0E34\u0E08\u0E01\u0E23\u0E23\u0E21"
+                })
+              })
+            }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("li", {
+              children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx(next_link__WEBPACK_IMPORTED_MODULE_2__.default, {
+                href: "/contact",
+                children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("button", {
+                  children: "\u0E15\u0E34\u0E14\u0E15\u0E48\u0E2D\u0E40\u0E23\u0E32"
+                })
+              })
+            })]
+          })
+        })
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("div", {
+        className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_4___default().footer3),
+        children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("iframe", {
+          src: "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fweb.facebook.com%2Fkhokprongwittayakhom%2Fphotos%2F%3Fref%3Dpage_internal&tabs=timeline&width=300&height=200&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId",
+          width: "300",
+          height: "200",
+          scrolling: "no",
+          frameborder: "0",
+          allowfullscreen: "true",
+          allow: "autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        })
+      })]
+    }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("div", {
+      className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_4___default().footer4),
+      children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("center", {
+        children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx("p", {
+          children: "Copyright @ 2021 Khokprongwittayakhom School All rights reserved"
+        })
+      })
+    })]
+  });
+}
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Footer);
+
+/***/ }),
+
+/***/ 1244:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "Z": () => (/* binding */ components_Nav)
+});
+
+// EXTERNAL MODULE: ./node_modules/next/link.js
+var next_link = __webpack_require__(1664);
+// EXTERNAL MODULE: ./styles/Nav.module.css
+var Nav_module = __webpack_require__(8371);
+var Nav_module_default = /*#__PURE__*/__webpack_require__.n(Nav_module);
+// EXTERNAL MODULE: ./node_modules/next/image.js
+var next_image = __webpack_require__(5675);
+;// CONCATENATED MODULE: ./public/logo-kp.png
+/* harmony default export */ const logo_kp = ({"src":"/_next/static/image/public/logo-kp.3e5028d940b16374d3457d2f288ee489.png","height":440,"width":400,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAAICAYAAAA1BOUGAAAA1ElEQVR42mOAA0sGpn1LHPx7DjAww8XuHd3BBKIvr+3PP9tcfPNwf3gsiH9iUS8Tw63bF8CSu9bN3rl5Qtr/VTMa1oD4+/csZYJo78p0Wzar+v+5BaW/V86v/a8+pcoHbrR6eUzTudnh/6fNKvp1dlHkf7uquHawxI+7DH2TVxjU9OX4PFhXHfW/Ljvi2cylcrWfzzNOY9g3L2zehUsB9zvXu+Yc3aR1bt52j+YLlwPvbpoSNxeo9yjv7r2Z6WfOJ506eT78wfkL8ed27c5JZGA4xQUAVT5gRrMClWsAAAAASUVORK5CYII="});
+// EXTERNAL MODULE: ./node_modules/react-icons/fi/index.esm.js
+var index_esm = __webpack_require__(6893);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(9297);
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(5282);
+;// CONCATENATED MODULE: ./components/Nav.js
+
+
+
+
+
+
+
+
+
+function Nav() {
+  const {
+    0: click,
+    1: setClick
+  } = (0,external_react_.useState)(false);
+
+  const handleClick = () => setClick(!click);
+
+  const {
+    0: nav,
+    1: setNav
+  } = (0,external_react_.useState)(false);
+  (0,external_react_.useEffect)(() => {
+    const changeBackground = () => {
+      if (window.scrollY > 100) {
+        setNav(true);
+      } else {
+        setNav(false);
+      }
+    };
+
+    window.addEventListener('scroll', changeBackground);
+    changeBackground();
+    return () => {
+      window.removeEventListener('scroll', changeBackground);
+    };
+  });
+  return /*#__PURE__*/(0,jsx_runtime_.jsxs)("nav", {
+    className: nav ? (Nav_module_default()).navactive : (Nav_module_default()).nav,
+    children: [/*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+      className: (Nav_module_default()).logo,
+      children: [/*#__PURE__*/jsx_runtime_.jsx("div", {
+        className: (Nav_module_default()).logokp,
+        children: /*#__PURE__*/jsx_runtime_.jsx(next_image.default, {
+          src: logo_kp,
+          alt: "logo",
+          width: 80,
+          height: 80
+        })
+      }), /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+        href: "/",
+        children: /*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+          className: (Nav_module_default()).Brand,
+          children: [/*#__PURE__*/jsx_runtime_.jsx("div", {
+            className: (Nav_module_default()).Brandh1,
+            children: /*#__PURE__*/jsx_runtime_.jsx("h1", {
+              children: "\u0E42\u0E23\u0E07\u0E40\u0E23\u0E35\u0E22\u0E19\u0E42\u0E04\u0E01\u0E1B\u0E23\u0E07\u0E27\u0E34\u0E17\u0E22\u0E32\u0E04\u0E21"
+            })
+          }), /*#__PURE__*/jsx_runtime_.jsx("div", {
+            className: (Nav_module_default()).Brandh2,
+            children: /*#__PURE__*/jsx_runtime_.jsx("h2", {
+              children: "Khokprongwittayakhom School"
+            })
+          })]
+        })
+      })]
+    }), /*#__PURE__*/(0,jsx_runtime_.jsxs)("ul", {
+      className: click ? (Nav_module_default()).menuactive : (Nav_module_default()).menu,
+      children: [/*#__PURE__*/jsx_runtime_.jsx("li", {
+        children: /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+          href: "/",
+          children: /*#__PURE__*/jsx_runtime_.jsx("button", {
+            children: "\u0E2B\u0E19\u0E49\u0E32\u0E41\u0E23\u0E01"
+          })
+        })
+      }), /*#__PURE__*/jsx_runtime_.jsx("li", {
+        children: /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+          href: "/school",
+          children: /*#__PURE__*/jsx_runtime_.jsx("button", {
+            children: "\u0E02\u0E49\u0E2D\u0E21\u0E39\u0E25\u0E42\u0E23\u0E07\u0E40\u0E23\u0E35\u0E22\u0E19"
+          })
+        })
+      }), /*#__PURE__*/jsx_runtime_.jsx("li", {
+        children: /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+          href: "/personal",
+          children: /*#__PURE__*/jsx_runtime_.jsx("button", {
+            children: "\u0E1A\u0E38\u0E04\u0E25\u0E32\u0E01\u0E23"
+          })
+        })
+      }), /*#__PURE__*/jsx_runtime_.jsx("li", {
+        children: /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+          href: "/student",
+          children: /*#__PURE__*/jsx_runtime_.jsx("button", {
+            children: "\u0E02\u0E49\u0E2D\u0E21\u0E39\u0E25\u0E19\u0E31\u0E01\u0E40\u0E23\u0E35\u0E22\u0E19"
+          })
+        })
+      }), /*#__PURE__*/jsx_runtime_.jsx("li", {
+        children: /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+          href: "/activity",
+          children: /*#__PURE__*/jsx_runtime_.jsx("button", {
+            children: "\u0E01\u0E34\u0E08\u0E01\u0E23\u0E23\u0E21"
+          })
+        })
+      }), /*#__PURE__*/jsx_runtime_.jsx("li", {
+        children: /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+          href: "/contact",
+          children: /*#__PURE__*/jsx_runtime_.jsx("button", {
+            children: "\u0E15\u0E34\u0E14\u0E15\u0E48\u0E2D\u0E40\u0E23\u0E32"
+          })
+        })
+      })]
+    }), /*#__PURE__*/jsx_runtime_.jsx("div", {
+      className: (Nav_module_default()).mobilemenu,
+      onClick: handleClick,
+      children: click ? /*#__PURE__*/jsx_runtime_.jsx(index_esm/* FiX */.q5L, {}) : /*#__PURE__*/jsx_runtime_.jsx(index_esm/* FiMenu */.cur, {})
+    })]
+  });
+}
+
+/* harmony default export */ const components_Nav = (Nav);
+
+/***/ }),
+
+/***/ 2202:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"footer00": "Footer_footer00__3q_3I",
+	"footer": "Footer_footer__317iK",
+	"footer1": "Footer_footer1__17Bwr",
+	"footer2": "Footer_footer2__cMBqy",
+	"footer3": "Footer_footer3__3kM85",
+	"footerbrand": "Footer_footerbrand__3lDqj",
+	"footer4": "Footer_footer4__3RZ5s"
+};
+
+
+/***/ }),
+
+/***/ 8371:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"nav": "Nav_nav__3Zjf-",
+	"navactive": "Nav_navactive__2Drvh",
+	"Brandh1": "Nav_Brandh1__1A-_r",
+	"Brandh2": "Nav_Brandh2__3Aum8",
+	"Brand": "Nav_Brand__35Wqc",
+	"logo": "Nav_logo__11byI",
+	"logokp": "Nav_logokp__1li4H",
+	"mobilemenu": "Nav_mobilemenu__pw3bw",
+	"menu": "Nav_menu__-y5zM",
+	"menuactive": "Nav_menuactive__3E-mk"
+};
+
+
+/***/ }),
+
+/***/ 2431:
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+};
+;
