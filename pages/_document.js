7f46e9d5346cb6c522bba601191cb04c4b49988b
Saridraj <saridrajsapsin@gmail.com>
2021-12-06 21:41:10 +0700

    init

--- pages/_document.js
+++ pages/_document.js
@@ -19,6 +19,36 @@ render() {
        <NextScript />
       
     </body>
+    <div id="fb-root"></div>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "107775365947740");
+      chatbox.setAttribute("attribution", "biz_inbox");
+
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v12.0'
+              });
+            };
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+            `,
+          }}
+        />
+
+        <div className="fb-customerchat"
+          attribution="page_inbox"
+          page_id="107775365947740">
+        </div>
+
     </Html>
   )
 }}
