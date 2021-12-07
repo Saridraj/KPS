7cbb65fb65b1eeb55ecc76863f53e40629a298fa
Saridraj <saridrajsapsin@gmail.com>
2021-12-07 22:29:29 +0700

    init

--- components/Facebook.js
+++ components/Facebook.js
@@ -6,7 +6,7 @@ function Facebook() {
       <div id="fb-root"></div>
 
       <div id="fb-customer-chat" className="fb-customerchat"></div>
-      <Script strategy="lazyOnload">
+      <Script src="https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js" strategy="lazyOnload">
         {`
             var chatbox = document.getElementById('fb-customer-chat');
             chatbox.setAttribute("page_id", "107775365947740");
