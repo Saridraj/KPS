28a502ec750751b8d5eb84f3151b54186ac80a63
Saridraj <saridrajsapsin@gmail.com>
2021-12-07 21:14:40 +0700

    init

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "740b9f6940ab888e"; }
+/******/ 		__webpack_require__.h = function() { return "9b0116b9e696057f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
