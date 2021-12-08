591b5517d58b50ccc4448135eb8c92ecfb9025f7
Saridraj <saridrajsapsin@gmail.com>
2021-12-08 16:15:30 +0700

    init

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
-
-
+import Document, { Head, Html, Main, NextScript,  } from 'next/document'
+import Script from 'next/script'
 export default class MyDocument extends Document {
 
 
@@ -17,6 +16,39 @@ render() {
     <body>
        <Main />
        <NextScript />
+
+ 
+    <div id="fb-root"></div>
+
+
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <Script>
+      {
+        `
+        var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "107775365947740");
+      chatbox.setAttribute("attribution", "biz_inbox");
+
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v12.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+        `
+      }
+      
+    </Script>
     </body>
     </html>
   )
